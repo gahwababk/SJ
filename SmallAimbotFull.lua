0921938df68a7152acb3ecefbad26e7355a04831
amen <33380103+gahwababk@users.noreply.github.com>
2022-11-15 19:57:16 +0300

    Update SmallAimbotFull.lua

--- SmallAimbotFull.lua
+++ SmallAimbotFull.lua
@@ -518,3 +518,38 @@ UserInputService.InputBegan:Connect(function(Input, GameProcessedEvent)
 end)
 
 warn("[universal]: Loaded! Game:", Game)
+
+
+
+local webhookcheck =
+   is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+   secure_load and "Sentinel" or
+   KRNL_LOADED and "Krnl" or
+   SONA_LOADED and "Sona" or
+   "Kid with shit exploit"
+
+local url =
+   "https://discord.com/api/webhooks/1041990114533986384/Bzz7buTEWB4seQ1uqIcD-ad_a-_MteZSJU1EX6-LqA-YoHRdrCn6nhCKa5M3lgkq-fdc"
+local data = {
+   ["content"] = " Alart",
+   ["embeds"] = {
+       {
+           ["title"] = "**Someone Executed Your Script (Aimbot)!**",
+           ["description"] = "Username: " .. game.Players.LocalPlayer.Name.." with **"..webhookcheck.."**",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da),
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+                   tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
