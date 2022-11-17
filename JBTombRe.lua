ce7e88cfb7a6e042701f55c0db2e3b9e7ea4b67c
amen <33380103+gahwababk@users.noreply.github.com>
2022-11-17 14:02:35 +0300

    Update JBTombRe.lua

--- JBTombRe.lua
+++ JBTombRe.lua
@@ -1,228 +1,159 @@
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
-
+local Thread1 = coroutine.create(function()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[1].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[1].Tile.Name = "Bruh1";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[2].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[2].Tile.Name = "Bruh1";
 
-end
 end)
 
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+coroutine.resume(Thread1)
+
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[2].Tile.Name = "Bruh2";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[2].Tile.Name = "Bruh3";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[3].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[3].Tile.Name = "Bruh1";
 
-end
-end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+local Thread1 = coroutine.create(function()
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[3].Tile.Name = "Bruh2";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[3].Tile.Name = "Bruh3";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[4].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[4].Tile.Name = "Bruh1";
 
-end
 end)
 
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+coroutine.resume(Thread1)
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[4].Tile.Name = "Bruh2";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[4].Tile.Name = "Bruh3";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[5].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[5].Tile.Name = "Bruh1";
 
-end
-end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+local Thread1 = coroutine.create(function()
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[6].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[6].Tile.Name = "Bruh1";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[6].Tile.Name = "Bruh2";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[6].Tile.Name = "Bruh3";
 
-end
 end)
 
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+coroutine.resume(Thread1)
+
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[7].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[7].Tile.Name = "Bruh1";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[7].Tile.Name = "Bruh2";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[7].Tile.Name = "Bruh3";
 
-end
-end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+local Thread1 = coroutine.create(function()
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[8].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[8].Tile.Name = "Bruh1";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[8].Tile.Name = "Bruh2";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[8].Tile.Name = "Bruh3";
 
-end
 end)
 
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+coroutine.resume(Thread1)
+
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[9].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[9].Tile.Name = "Bruh1";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[10].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[10].Tile.Name = "Bruh1";
 
-
-end
-end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+local Thread1 = coroutine.create(function()
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[10].Tile.Name = "Bruh2";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[10].Tile.Name = "Bruh3";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[11].Tile.Name = "Bruh";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[11].Tile.Name = "Bruh1";
 
-end
 end)
 
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+coroutine.resume(Thread1)
+
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[11].Tile.Name = "Bruh2";
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes[11].Tile.Name = "Bruh3";
 
-end
-end)
-
-
----
-
-
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+local Thread1 = coroutine.create(function()
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["1"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["1"].Bruh1.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["2"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["2"].Bruh1.Model.InnerModel:Destroy()
 
-end
 end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+
+coroutine.resume(Thread1)
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["2"].Bruh2.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["2"].Bruh3.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["3"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["3"].Bruh1.Model.InnerModel:Destroy()
 
-end
-end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+local Thread1 = coroutine.create(function()
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["3"].Bruh2.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["3"].Bruh3.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["4"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["4"].Bruh1.Model.InnerModel:Destroy()
 
-end
 end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+
+coroutine.resume(Thread1)
+
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["4"].Bruh2.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["4"].Bruh3.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["5"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["5"].Bruh1.Model.InnerModel:Destroy()
 
-end
-end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+local Thread1 = coroutine.create(function()
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["6"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["6"].Bruh1.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["6"].Bruh2.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["6"].Bruh3.Model.InnerModel:Destroy()
 
-end
 end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+
+coroutine.resume(Thread1)
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["7"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["7"].Bruh1.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["7"].Bruh2.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["7"].Bruh3.Model.InnerModel:Destroy()
 
-end
-end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+local Thread1 = coroutine.create(function()
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["8"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["8"].Bruh1.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["8"].Bruh2.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["8"].Bruh3.Model.InnerModel:Destroy()
 
-end
 end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+
+coroutine.resume(Thread1)
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["9"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["9"].Bruh1.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["10"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["10"].Bruh1.Model.InnerModel:Destroy()
 
-end
-end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
-
+local Thread1 = coroutine.create(function()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["10"].Bruh2.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["10"].Bruh3.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["11"].Bruh.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["11"].Bruh1.Model.InnerModel:Destroy()
 
-end
 end)
-spawn(function()
-for i,v in pairs(workspace:GetChildren()) do
-wait()
+
+coroutine.resume(Thread1)
 
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["11"].Bruh2.Model.InnerModel:Destroy()
 game:GetService("Workspace").RobberyTomb.SpikeRoom.Spikes["11"].Bruh3.Model.InnerModel:Destroy()
-
-end
-end)
 game:GetService("Workspace").RobberyTomb.DartRoom.Darts:remove()
